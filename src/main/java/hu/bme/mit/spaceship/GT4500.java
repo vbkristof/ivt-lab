9a375da7fcfa34ffa2d6ac79a8ec6d115d416fed
Kristof Varga-Balazs <kristofvargabalazs@gmail.com>
2023-04-21 09:04:45 +0200

    create conflict on B

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -77,7 +77,7 @@ public class GT4500 implements SpaceShip {
         break;
 
       case ALL:
-        // try to fire both of the torpedo stores
+        // try to fire both of the torpedo stores THIS GOING TO CAUSE MERGE CONFLICT2
         firingSuccess = true;
         break;
     }
